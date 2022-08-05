c091d00748099d9339c052c07ae30a3677f68b1a
Mábio Rodrigues <mabio.rodrigues@outlook.com>
2022-08-05 09:58:09 -0300

    First commit: project structure created

---
+++ src/main/java/com/mabio/boookstoremanager/BoookstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mabio.boookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoookstoremanagerApplication.class, args);
+	}
+
+}
